5de3df62ee2a3a6ca989df2b4b6e1e1cbafef562
willxiaochen <willxiaochen@gmail.com>
2013-10-12 16:35:38 -0700

    delete after merger

--- src/coo/be/toy/MainActivity.java
+++ src/coo/be/toy/MainActivity.java
@@ -12,8 +12,11 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         
         TextView text = new TextView(this);
-        text.setText("Greeting from Daming\n");
+
+        String s = "Greeting from Daming\n"+"Say your greetings below:\n";
         text.append("Greating from Xiaochen!\n");
+        
+        text.setText(s);
         setContentView(text);  
         
         //setContentView(R.layout.activity_main);
